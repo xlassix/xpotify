9018285f64a3b9652cce52c0059abf66b422a761
xlassix <46370698+xlassix@users.noreply.github.com>
2022-04-18 00:13:02 +0100

    feat: api routes

---
+++ lib/hooks.ts
@@ -0,0 +1,20 @@
+import useSWR from "swr";
+import fetcher from "./fetcher";
+
+export const useMe = () => {
+  const { data, error } = useSWR("/me", fetcher);
+  return {
+    user: data,
+    isLoading: !data && !error,
+    error,
+  };
+};
+
+export const usePlaylist = () => {
+  const { data, error } = useSWR("/playlist", fetcher);
+  return {
+    playlist: (data as any) || [],
+    isLoading: !data && !error,
+    error,
+  };
+};
---
+++ pages/_middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const protectRoute = ["/", "/playlist", "/library"];
+
+export default function middleware(req: NextRequest) {
+  if (protectRoute.find((p) => p === req.nextUrl.pathname)) {
+    if (!req.cookies.DELIVAME_ACCESS_TOKEN) {
+      return NextResponse.redirect("/auth");
+    }
+  }
+}
